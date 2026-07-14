787f24d2bf6674e66345f9d632bc3591af75004a
ale <alexisrivas2000@proton.me>
2026-07-14 15:36:26 -0500

    added docker lsps

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,6 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls" }
+local servers = { "html", "cssls", "dockerls", "docker_compose_language_service" }
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers 
